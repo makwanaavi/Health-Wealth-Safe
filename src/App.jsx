323905e76376eb164aa4ef2494c31636c8f4dd03
makwanaavi <makwanaavi73@gmail.com>
2025-06-30 11:08:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LoginPage from './components/ui/LoginPage'
+
+function App() {
+
+  return (
+    <>
+    {/* <h2>Health Wealth Safe</h2> */}
+    <LoginPage />
+    </>
+  )
+}
+
+export default App
