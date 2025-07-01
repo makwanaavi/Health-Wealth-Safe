b67c7b20a7223b0ce39087eb75322c300cb0dba4
makwanaavi <makwanaavi73@gmail.com>
2025-07-01 16:12:03 +0530

    #UPDATE

--- src/components/ui/Dashboard.jsx
+++ src/components/ui/Dashboard.jsx
@@ -1,16 +1,26 @@
+import FeelingChart from "../FeelingChart";
 import Header from "../Header";
 import Navbar from "../Navbar";
 
 const Dashboard = () => {
-  
   return (
     <div className="min-h-screen bg-gray-100">
       <header className="bg-white shadow">
-       <Header/>
+        <Header />
       </header>
       <main>
         <div>
-          <Navbar/>
+          <Navbar />
+          {/* Card container to prevent overlap and allow smooth drag */}
+          <div
+            className="relative flex flex-wrap   gap-6 mt-6 px-4"
+            style={{ minHeight: 400, alignItems: "flex-start" }}
+          >
+            <div className="relative flex-1 min-w-[320px] max-w-[650px]">
+              <FeelingChart />
+            </div>
+           
+          </div>
         </div>
       </main>
     </div>
@@ -18,3 +28,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
