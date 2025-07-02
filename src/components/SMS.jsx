abe68471c5bcf43899942ec6b2f421038698f1a9
makwanaavi <makwanaavi73@gmail.com>
2025-07-02 11:42:13 +0530

    #update

--- src/components/SMS.jsx
+++ src/components/SMS.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
 const SMS = () => {
-  return (
-    <div>
-      SMS
-    </div>
-  )
-}
-  
-export default SMS
 
+  return (
+      <div className="bg-blue-100 w-full h-[700px] rounded shadow-md  flex flex-col">
+        <div className="p-3 text-[#3699FF] font-semibold">SMS</div>
+        <div className="flex-1 overflow-y-auto p-4 bg-white">
+      
+        </div>
+        <div className="p-3 flex gap-2 items-center bg-white">
+       
+        </div>
+      </div>
+  );
+};
+
+export default SMS;
