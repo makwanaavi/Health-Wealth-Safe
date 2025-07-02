0552c14f61434192ee757f9420acedf3c08f5d1c
makwanaavi <makwanaavi73@gmail.com>
2025-07-02 15:14:14 +0530

    #update

--- src/components/VideoCall.jsx
+++ src/components/VideoCall.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const VideoCall = () => {
   return (
     <div className='flex justify-center gap-12'>
-     <div className='w-80 h-60 bg-gray-200 flex justify-center items-center mt-12'>
-      <button className='bg-blue-400 p-4 cursor-pointer'>START YOUR CALL NOW</button>
+     <div className='w-80 h-60 bg-white flex justify-center items-center mt-12 shadow-xl'>
+      <button className='bg-blue-50 text-[#3699FF] p-4 cursor-pointer '>
+        <Link to='/exam-room'>START YOUR CALL NOW</Link>
+      </button>
      </div>
-     <div className='w-80 h-60 bg-gray-400 flex justify-center items-center mt-12'> 
-       <button className='bg-blue-400 p-4 cursor-pointer'>START YOUR CALL NOW</button>
+     <div className='w-80 h-60 bg-white flex justify-center items-center mt-12 shadow-xl'> 
+       <button className='bg-blue-50 text-[#3699FF]   p-4 cursor-pointer'> SCHEDULE YOUR CALL NOW </button>
      </div>
     </div>
   )
