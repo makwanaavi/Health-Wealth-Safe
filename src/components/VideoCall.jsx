675a520a857a744fd07eee87c43e20d7000120aa
makwanaavi <makwanaavi73@gmail.com>
2025-07-02 14:03:18 +0530

    #update

--- src/components/VideoCall.jsx
+++ src/components/VideoCall.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const VideoCall = () => {
   return (
-    <div>
-      VideoCall
+    <div className='flex justify-center gap-12'>
+     <div className='w-80 h-60 bg-gray-200 flex justify-center items-center mt-12'>
+      <button className='bg-blue-400 p-4 cursor-pointer'>START YOUR CALL NOW</button>
+     </div>
+     <div className='w-80 h-60 bg-gray-400 flex justify-center items-center mt-12'> 
+       <button className='bg-blue-400 p-4 cursor-pointer'>START YOUR CALL NOW</button>
+     </div>
     </div>
   )
 }
